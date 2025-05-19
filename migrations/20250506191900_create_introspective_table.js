c8688eb5a462c61ce1d640dfeb71786ea9620735
matt secondi <matt_secondi@hotmail.com>
2025-05-19 12:09:25 -0400

    clarified structure of migration tables

--- migrations/20250506191900_create_introspective_table.js
+++ migrations/20250506191900_create_introspective_table.js
@@ -3,22 +3,17 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-    return knex.schema
-      .createTable("introspective", (table) => {
-        table.increments("id").primary();
-        table.string("name"); // allow users to input data anonymously 
-        table.string("content").notNullable();
-        table.timestamp("created_at").defaultTo(knex.fn.now());
-        table
-          .timestamp("updated_at")
-          .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
-      })
-  }
-  
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Promise<void> }
-   */
-  export function down(knex) {
-    return knex.schema.dropTable("post").dropTable("user");
-  }
+  return knex.schema.createTable("introspective", (table) => {
+    table.increments("id").primary();
+    table.date("date").notNullable(); // store the date of the reflection -- to be used on frontend to see how many reflections were made TODAY
+    table.timestamp("created_at").defaultTo(knex.fn.now()); // optional: for tracking exact time if ever needed
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("introspective");
+}
